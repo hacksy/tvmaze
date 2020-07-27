f2223506cc917464398fd17386fffddd5a61e43e
0xhacksy <me@hansyschmitt.com>
2020-07-27 13:34:49 -0500

    Add DataSource for TvSeries

--- app/src/main/java/com/hacksy/tvmaze/data/db/DbDataSource.kt
+++ app/src/main/java/com/hacksy/tvmaze/data/db/DbDataSource.kt
@@ -1,2 +1,9 @@
 package com.hacksy.tvmaze.data.db
 
+import androidx.lifecycle.LiveData
+
+interface DbDataSource {
+    fun tvSeries(): LiveData<List<TvSeriesDTO>>
+    suspend fun addTvSeries(museums: List<TvSeriesDTO>)
+    suspend fun deleteTVSeries()
+}
